bcf6d73c856ec6a58c7200065febc536cfb2f6de
Minhokg <90128043+Minhokg@users.noreply.github.com>
2023-03-04 00:05:29 +0900

    Update KEPCO Data Analysis by Deep Learning.py

--- KEPCO Data Analysis by Deep Learning.py
+++ KEPCO Data Analysis by Deep Learning.py
@@ -1,3 +1,4 @@
+# Import every library that We will use 
 from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf
 from keras.models import Model
@@ -20,3 +21,63 @@ from ipywidgets import interact, interact_manual
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Use 'warnings' to avoid warning messages 
+warnings.filterwarnings('ignore')
+
+# import KEPCO(Korea Electric Power Corporation) data 
+Kepco = stock.get_market_ohlcv_by_date(fromdate="20110101", todate="20211210", ticker="015760")
+
+# Diagnose whether there are null data 
+Kepco.isnull().sum()
+
+# Set the first data and last date as below 
+from datetime import datetime
+time_first=datetime(2011,1,3)
+time_last = datetime(2021,12,10)
+
+# And we can visualize each column of data like market place by using 'interact' function
+def f(col):
+    col_value=Kepco[col]
+    plt.plot(col_value)
+interact(f, col=list(Kepco.columns))
+
+# This time, let's divide data into train and test. The train ratio is 0.7
+train_ratio = 0.7
+train_len = int(train_ratio * Kepco.shape[0])
+train_stock = Kepco[:train_len]
+test_stock = Kepco[train_len:]
+
+# Do scaling train data by using MinMaxScaler 
+train = train_stock.copy()
+scalers = {}
+for i in train_stock.columns:
+    scaler = MinMaxScaler(feature_range=(-1,1))
+    s_s = scaler.fit_transform(train[i].values.reshape(-1,1))
+    scalers['scaler_'+i]=scaler
+    train[i] = s_s
+    
+# Likewise, do scaling test data too
+test = test_stock.copy()
+for i in train_stock.columns:
+    scaler = scalers['scaler_'+i]
+    s_s = scaler.transform(test[i].values.reshape(-1,1))
+    test[i] = s_s
+    
+    
+# Next, the below function is for making time series data. 
+def split_series(series, n_past,n_future,target_col=list(range(len(Kepco.columns)))):
+    X, y = list(), list()
+    for window_start in range(len(series)):
+        past_end = window_start + n_past
+        future_end = past_end + n_future
+        if future_end > len(series):
+            break
+        past, future = series[window_start:past_end,:], series[past_end:future_end,target_col]
+        X.append(past)
+        y.append(future)
+    return np.array(X), np.array(y)
+            
+# First, Let me use LSTM Model.
+
+
